bdb6f2f44c34f1f9b44424f26130454094759abf
hashirSoomro <hashirsomroo@gmail.com>
2024-03-26 19:57:27 +0500

    Create Drawer Widget for Admin Panel

---
+++ admin_panel/lib/screens/main-screens.dart
@@ -0,0 +1,26 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+import '../utils/app-constant.dart';
+import '../widgets/custom-drawer-widget.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppConstant.appMainColor,
+        title: Text("Admin Panel"),
+      ),
+      drawer: DrawerWidget(),
+    );
+  }
+}
